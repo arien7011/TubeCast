b908f9ee33d76dccc9372484cca6cc873e1a76df
arien7011 <amanmishra01234@gmail.com>
2024-09-10 22:56:38 +0530

    add user account management endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -261,4 +261,40 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
     res.status(200).json(new apiResponse(200,{user},"User Updated Successfully"));
 })
 
-export {registerUser , loginUser ,logoutUser,VerifyRefreshToken,changeUserPassword,getCurrentUser,updateAccountDetails};
+const uploadAvatarImage = asyncHandler(async(req,res)=>{
+  try{
+    const avatarLocalPath = req.file?.path;
+    if(!avatarLocalPath){
+     throw new ApiError(400,"Avatar file is missing");
+    }
+  const avatar =   await uploadOnCloudinary(avatarLocalPath);
+  if(!avatar){
+    throw new ApiError(400,"Upload on cloudinary failed");
+  }
+   const user = await User.findByIdAndUpdate(req?.user?._id,{$set:{avatar:avatar.url}},{new:true}).select(" -password");
+  return  res.status(200).json(new ApiResponse(200,{user},"File Updated Successfully"))
+  }catch(error){
+    throw new ApiError(400,"File Upload failed")
+  }
+ 
+})
+
+const uploadCoverImage = asyncHandler(async(req,res)=>{
+  try{
+    const coverImageLocalPath =  req.file?.path;
+    if(!coverImageLocalPath){
+     throw new ApiError(400,"Cover Image file not found");
+    }
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    if(!coverImage){
+      throw new ApiError(400,"Error while uploading cover Image on cloudinary");
+    }
+    const user = await User.findByIdAndUpdate(req.user._id,{$set:{coverImage:coverImage?.url}},{new:true}).select("-password");
+    return res.status(200).json(200,{user},"Cover Image Updated Successfully")
+  } catch(error){
+    throw new ApiError(400,"Error while updating cover Image"); 
+  }
+ 
+})
+
+export {registerUser , loginUser ,logoutUser,VerifyRefreshToken,changeUserPassword,getCurrentUser,updateAccountDetails,uploadAvatarImage,uploadCoverImage};
